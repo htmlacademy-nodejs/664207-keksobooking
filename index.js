5d874f51a6040a389d9da3bc93eb0aa4739362d2
vladimir-rurik <36802394+vladimir-rurik@users.noreply.github.com>
2018-09-28 14:48:58 +0300

    modules

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
+const execute = require(`./src/execute`);
+
 const [arg] = process.argv.slice(2);
 
-if (arg === `--version`) {
-  console.log(`v0.0.1`);
-  process.exit(0);
-}
-
-if (arg === `--help`) {
-  console.log(`Доступные команды:`);
-  console.log(`--help — печатает этот текст`);
-  console.log(`--version — печатает версию приложения)`);
-  process.exit(0);
-}
-
 if (!arg) {
-  console.log(`Привет пользователь!`);
-  console.log(`Эта программа будет запускать сервер Keksobooking.`);
-  console.log(`Автор: Кекс.`);
+  execute.empty();
   process.exit(0);
 }
 
-console.log(`Неизвестная команда ${arg}.`);
-console.error(`Чтобы прочитать правила использования приложения, наберите "--help"`);
+if (execute[arg]) {
+  execute[arg]();
+  process.exit(0);
+}
+
+execute.unknownCommand(arg);
 process.exit(1);
---
+++ src/author.js
@@ -0,0 +1,9 @@
+const packageInfo = require(`../package.json`);
+
+module.exports = {
+  name: `author`,
+  description: `Shows the author of the application`,
+  execute() {
+    console.log(packageInfo.author);
+  }
+};
