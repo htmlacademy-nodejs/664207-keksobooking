086af1b993868bbcf0578e75c59e393a13708b77
vladimir-rurik <36802394+vladimir-rurik@users.noreply.github.com>
2018-09-28 19:53:00 +0300

    colored output

--- src/version.js
+++ src/version.js
@@ -1,9 +1,20 @@
+const colors = require(`colors/safe`);
 const packageInfo = require(`../package.json`);
 
 module.exports = {
   name: `version`,
   description: `Shows the version of the application`,
   execute() {
-    console.log(`v${packageInfo.version}`);
+    const versionColors = [`red`, `green`, `blue`];
+    const versionParts = packageInfo.version.split(`.`);
+
+    process.stdout.write(`v`);
+    versionParts.map((versionPart, index) => {
+      const coloredMethod = colors[versionColors[index]].bind(colors);
+      process.stdout.write(coloredMethod(versionPart));
+      if (index < versionParts.length - 1) {
+        process.stdout.write(`.`);
+      }
+    });
   }
 };
